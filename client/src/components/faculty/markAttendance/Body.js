c798c4e194185770835cded9fecdb5cf4b4402e0
sarthak783891 <gsarthak783891@gmail.com>
2023-12-03 22:58:05 +0530

    some more changes

--- client/src/components/faculty/markAttendance/Body.js
+++ client/src/components/faculty/markAttendance/Body.js
@@ -1,67 +1,67 @@
-import React, { useEffect, useState } from "react";
-import BoyIcon from "@mui/icons-material/Boy";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import BoyIcon from '@mui/icons-material/Boy'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   getStudent,
   markAttendance,
-} from "../../../redux/actions/facultyActions";
-import { MenuItem, Select } from "@mui/material";
-import Spinner from "../../../utils/Spinner";
-import * as classes from "../../../utils/styles";
-import { ATTENDANCE_MARKED, SET_ERRORS } from "../../../redux/actionTypes";
-import { getTest } from "../../../redux/actions/facultyActions";
-import { getSubject } from "../../../redux/actions/adminActions";
+} from '../../../redux/actions/facultyActions'
+import { MenuItem, Select } from '@mui/material'
+import Spinner from '../../../utils/Spinner'
+import * as classes from '../../../utils/styles'
+import { ATTENDANCE_MARKED, SET_ERRORS } from '../../../redux/actionTypes'
+import { getTest } from '../../../redux/actions/facultyActions'
+import { getSubject } from '../../../redux/actions/adminActions'
 const Body = () => {
-  const dispatch = useDispatch();
-  const user = JSON.parse(localStorage.getItem("user"));
-  const departments = useSelector((state) => state.admin.allDepartment);
-  const subjects = useSelector((state) => state.admin.subjects.result);
+  const dispatch = useDispatch()
+  const user = JSON.parse(localStorage.getItem('user'))
+  const departments = useSelector((state) => state.admin.allDepartment)
+  const subjects = useSelector((state) => state.admin.subjects.result)
 
-  const [error, setError] = useState({});
-  const [loading, setLoading] = useState(false);
-  const store = useSelector((state) => state);
-  const [subjectName, setSubjectName] = useState("");
-  const [checkedValue, setCheckedValue] = useState([]);
+  const [error, setError] = useState({})
+  const [loading, setLoading] = useState(false)
+  const store = useSelector((state) => state)
+  const [subjectName, setSubjectName] = useState('')
+  const [checkedValue, setCheckedValue] = useState([])
 
   const [value, setValue] = useState({
-    department: "",
-    year: "",
-    section: "",
-  });
-  const [search, setSearch] = useState(false);
+    department: '',
+    year: '',
+    section: '',
+  })
+  const [search, setSearch] = useState(false)
 
   useEffect(() => {
     if (Object.keys(store.errors).length !== 0) {
-      setError(store.errors);
-      setLoading(false);
-      setValue({ department: "", year: "", section: "" });
+      setError(store.errors)
+      setLoading(false)
+      setValue({ department: '', year: '', section: '' })
     }
-  }, [store.errors]);
+  }, [store.errors])
 
   const handleInputChange = (e) => {
-    const tempCheck = checkedValue;
-    let index;
+    const tempCheck = checkedValue
+    let index
     if (e.target.checked) {
-      tempCheck.push(e.target.value);
+      tempCheck.push(e.target.value)
     } else {
-      index = tempCheck.indexOf(e.target.value);
-      tempCheck.splice(index, 1);
+      index = tempCheck.indexOf(e.target.value)
+      tempCheck.splice(index, 1)
     }
-    setCheckedValue(tempCheck);
-  };
+    setCheckedValue(tempCheck)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearch(true);
-    setLoading(true);
-    setError({});
-    dispatch(getStudent(value));
-    dispatch(getSubject({ department: value.department, year: value.year }));
-  };
-  const students = useSelector((state) => state.admin.students.result);
+    e.preventDefault()
+    setSearch(true)
+    setLoading(true)
+    setError({})
+    dispatch(getStudent(value))
+    dispatch(getSubject({ department: value.department, year: value.year }))
+  }
+  const students = useSelector((state) => state.admin.students.result)
 
   const uploadAttendance = (e) => {
-    setError({});
+    setError({})
     dispatch(
       markAttendance(
         checkedValue,
@@ -70,113 +70,121 @@ const Body = () => {
         value.year,
         value.section
       )
-    );
-  };
+    )
+  }
 
   useEffect(() => {
     if (store.errors || store.faculty.attendanceUploaded) {
-      setLoading(false);
+      setLoading(false)
       if (store.faculty.attendanceUploaded) {
-        setValue({ department: "", year: "", section: "" });
-        setSearch(false);
-        setSubjectName("");
-        dispatch({ type: SET_ERRORS, payload: {} });
-        dispatch({ type: ATTENDANCE_MARKED, payload: false });
+        setValue({ department: '', year: '', section: '' })
+        setSearch(false)
+        setSubjectName('')
+        dispatch({ type: SET_ERRORS, payload: {} })
+        dispatch({ type: ATTENDANCE_MARKED, payload: false })
       }
     } else {
-      setLoading(true);
+      setLoading(true)
     }
-  }, [store.errors, store.faculty.attendanceUploaded]);
+  }, [store.errors, store.faculty.attendanceUploaded])
 
   useEffect(() => {
     if (store.faculty.attendanceUploaded) {
-      setValue({ department: "", year: "", section: "" });
+      setValue({ department: '', year: '', section: '' })
     }
-  }, [store.faculty.attendanceUploaded]);
+  }, [store.faculty.attendanceUploaded])
 
   useEffect(() => {
-    if (students?.length !== 0) setLoading(false);
-  }, [students]);
+    if (students?.length !== 0) setLoading(false)
+  }, [students])
 
   useEffect(() => {
-    dispatch({ type: SET_ERRORS, payload: {} });
-  }, []);
+    dispatch({ type: SET_ERRORS, payload: {} })
+  }, [])
 
   return (
-    <div className="flex-[0.8] mt-3">
-      <div className="space-y-5">
-        <div className="flex text-gray-400 items-center space-x-2">
+    <div className='flex-[0.8] mt-3'>
+      <div className='space-y-5'>
+        <div className='flex items-center space-x-2 text-gray-400'>
           <BoyIcon />
           <h1>All Students</h1>
         </div>
-        <div className=" mr-10 bg-white grid grid-cols-4 rounded-xl pt-6 pl-6 h-[29.5rem]">
+        <div className=' mr-10 bg-white grid grid-cols-4 rounded-xl pt-6 pl-6 h-[29.5rem]'>
           <form
-            className="flex flex-col space-y-2 col-span-1"
-            onSubmit={handleSubmit}>
-            <label htmlFor="department">Department</label>
+            className='flex flex-col col-span-1 space-y-2'
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor='department'>Department</label>
             <Select
               required
               displayEmpty
               sx={{ height: 36, width: 224 }}
-              inputProps={{ "aria-label": "Without label" }}
+              inputProps={{ 'aria-label': 'Without label' }}
               value={value.department}
               onChange={(e) =>
                 setValue({ ...value, department: e.target.value })
-              }>
-              <MenuItem value="">None</MenuItem>
+              }
+            >
+              <MenuItem value=''>None</MenuItem>
               {departments?.map((dp, idx) => (
-                <MenuItem key={idx} value={dp.department}>
+                <MenuItem
+                  key={idx}
+                  value={dp.department}
+                >
                   {dp.department}
                 </MenuItem>
               ))}
             </Select>
-            <label htmlFor="year">Year</label>
+            <label htmlFor='year'>Year</label>
             <Select
               required
               displayEmpty
               sx={{ height: 36, width: 224 }}
-              inputProps={{ "aria-label": "Without label" }}
+              inputProps={{ 'aria-label': 'Without label' }}
               value={value.year}
-              onChange={(e) => setValue({ ...value, year: e.target.value })}>
-              <MenuItem value="">None</MenuItem>
-              <MenuItem value="1">1</MenuItem>
-              <MenuItem value="2">2</MenuItem>
-              <MenuItem value="3">3</MenuItem>
-              <MenuItem value="4">4</MenuItem>
+              onChange={(e) => setValue({ ...value, year: e.target.value })}
+            >
+              <MenuItem value=''>None</MenuItem>
+              <MenuItem value='1'>1</MenuItem>
+              <MenuItem value='2'>2</MenuItem>
+              <MenuItem value='3'>3</MenuItem>
+              <MenuItem value='4'>4</MenuItem>
             </Select>
-            <label htmlFor="section">Section</label>
+            <label htmlFor='section'>Section</label>
             <Select
               required
               displayEmpty
               sx={{ height: 36, width: 224 }}
-              inputProps={{ "aria-label": "Without label" }}
+              inputProps={{ 'aria-label': 'Without label' }}
               value={value.section}
-              onChange={(e) => setValue({ ...value, section: e.target.value })}>
-              <MenuItem value="">None</MenuItem>
-              <MenuItem value="1">1</MenuItem>
-              <MenuItem value="2">2</MenuItem>
-              <MenuItem value="3">3</MenuItem>
+              onChange={(e) => setValue({ ...value, section: e.target.value })}
+            >
+              <MenuItem value=''>None</MenuItem>
+              <MenuItem value='1'>1</MenuItem>
+              <MenuItem value='2'>2</MenuItem>
+              <MenuItem value='3'>3</MenuItem>
             </Select>
 
             <button
               className={`${classes.adminFormSubmitButton} w-56`}
-              type="submit">
+              type='submit'
+            >
               Search
             </button>
           </form>
-          <div className="col-span-3 mr-6">
+          <div className='col-span-3 mr-6'>
             <div className={classes.loadingAndError}>
               {loading && (
                 <Spinner
-                  message="Loading"
+                  message='Loading'
                   height={50}
                   width={150}
-                  color="#111111"
-                  messageColor="blue"
+                  color='#111111'
+                  messageColor='blue'
                 />
               )}
               {(error.noStudentError || error.backendError) && (
-                <p className="text-red-500 text-2xl font-bold">
+                <p className='text-2xl font-bold text-red-500'>
                   {error.noStudentError || error.backendError}
                 </p>
               )}
@@ -186,7 +194,7 @@ const Body = () => {
               Object.keys(error).length === 0 &&
               students?.length !== 0 && (
                 <div className={`${classes.adminData} h-[20rem]`}>
-                  <div className="grid grid-cols-7">
+                  <div className='grid grid-cols-7'>
                     <h1 className={`col-span-1 ${classes.adminDataHeading}`}>
                       Select
                     </h1>
@@ -207,28 +215,33 @@ const Body = () => {
                   {students?.map((stu, idx) => (
                     <div
                       key={idx}
-                      className={`${classes.adminDataBody} grid-cols-7`}>
+                      className={`${classes.adminDataBody} grid-cols-7`}
+                    >
                       <input
                         onChange={handleInputChange}
                         value={stu._id}
-                        className="col-span-1 border-2 w-16 h-4 mt-3 px-2 "
-                        type="checkbox"
+                        className='w-16 h-4 col-span-1 px-2 mt-3 border-2 '
+                        type='checkbox'
                       />
                       <h1
-                        className={`col-span-1 ${classes.adminDataBodyFields}`}>
+                        className={`col-span-1 ${classes.adminDataBodyFields}`}
+                      >
                         {idx + 1}
                       </h1>
                       <h1
-                        className={`col-span-2 ${classes.adminDataBodyFields}`}>
+                        className={`col-span-2 ${classes.adminDataBodyFields}`}
+                      >
                         {stu.name}
                       </h1>
                       <h1
-                        className={`col-span-2 ${classes.adminDataBodyFields}`}>
+                        className={`col-span-2 ${classes.adminDataBodyFields}`}
+                      >
                         {stu.username}
                       </h1>
 
                       <h1
-                        className={`col-span-1 ${classes.adminDataBodyFields}`}>
+                        className={`col-span-1 ${classes.adminDataBodyFields}`}
+                      >
                         {stu.section}
                       </h1>
                     </div>
@@ -236,25 +249,30 @@ const Body = () => {
                 </div>
               )}
             {search && Object.keys(error).length === 0 && (
-              <div className="space-x-3 flex items-center justify-center mt-5">
-                <label className="font-bold text-lg">Subject</label>
+              <div className='flex items-center justify-center mt-5 space-x-3'>
+                <label className='text-lg font-bold'>Subject</label>
                 <Select
                   required
                   displayEmpty
                   sx={{ height: 36, width: 224 }}
-                  inputProps={{ "aria-label": "Without label" }}
+                  inputProps={{ 'aria-label': 'Without label' }}
                   value={subjectName}
-                  onChange={(e) => setSubjectName(e.target.value)}>
-                  <MenuItem value="">None</MenuItem>
+                  onChange={(e) => setSubjectName(e.target.value)}
+                >
+                  <MenuItem value=''>None</MenuItem>
                   {subjects?.map((dp, idx) => (
-                    <MenuItem key={idx} value={dp.subjectName}>
+                    <MenuItem
+                      key={idx}
+                      value={dp.subjectName}
+                    >
                       {dp.subjectName}
                     </MenuItem>
                   ))}
                 </Select>
                 <button
                   onClick={uploadAttendance}
-                  className={`${classes.adminFormSubmitButton} bg-blue-500`}>
+                  className={`${classes.adminFormSubmitButton} bg-blue-500`}
+                >
                   Mark
                 </button>
               </div>
@@ -263,7 +281,7 @@ const Body = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Body;
+export default Body
